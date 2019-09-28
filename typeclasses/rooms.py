dc98bdb9c79ed53fa778ae917c903d90ee3f1960
Earthcrusher <brendon.hollesen@gmail.com>
2019-09-28 11:43:00 -0400

    Oops, forgot to delete commented code

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -114,7 +114,6 @@ class Room(DefaultRoom):
             err_msg = err_msg % f"There is no {dir}ward exit."
             return False, err_msg
 
-        # self.db.exits[dir] = None
         self.db.exits[dir].delete()
         return True, ""
 
